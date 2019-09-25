a28cff15f21a53e501e18919a0d24d9a86a8d4c9
Pickle Dan <PickleDan@yandex.ru>
2019-09-25 16:19:04 +0300

    start to do <enough is enough> challenge. task from CodeWars

--- js/step1/main.js
+++ js/step1/main.js
@@ -1,3 +1,20 @@
-function selectedItem() {
+const items = document.getElementsByClassName('sort__item');
 
+console.log(items);
+
+for (let item of items) {
+    item.addEventListener('click', fireActive);
+    item.addEventListener('mouseenter', toggleBackground);
+    item.addEventListener('mouseleave', toggleBackground);
+}
+
+function fireActive(el) {
+    for (let item of items) {
+        item.classList.remove('active');
+    }
+    el.target.classList.add('active');
+}
+
+function toggleBackground(el) {
+    el.target.clallList.toggle('selected');
 }
