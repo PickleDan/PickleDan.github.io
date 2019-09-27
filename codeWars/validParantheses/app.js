4c8d1a2e07ce00c8b3384a1cf171cc05df5f4af3
Pickle Dan <PickleDan@yandex.ru>
2019-09-27 11:47:52 +0300

    finished Valid Paranthese task

---
+++ codeWars/validParantheses/app.js
@@ -0,0 +1,17 @@
+function validParentheses(parens) {
+    const open = ['('];
+    const close = [')'];
+    const stack = [];
+    for (let i = 0; i < parens.length; i++) {
+        if (open.includes(parens[i])) {
+            stack.push(parens[i]);
+        } else {
+            if (close.indexOf(parens[i]) == open.indexOf(stack[stack.length - 1])) {
+                stack.pop();
+            } else return false;
+        }
+    }
+    return stack.length == 0;
+}
+
+console.log(validParentheses("(()"));
